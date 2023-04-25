14b8d215698e028215cb10c2ffbd283f45ac3a1b
QY Li <m13386474590@163.com>
2023-04-25 20:26:43 +0800

    完成

--- sukodu.cpp
+++ sukodu.cpp
@@ -1,10 +1,20 @@
 ﻿#include <iostream>
 #include <ctime>
+#include <vector>
 #include <stdlib.h>
 #include <conio.h>
 #include <Windows.h>
 #include <thread>
 using namespace std;
+
+struct HistoryOption
+{
+	int row;
+	int col;
+	int option;
+	int oldData;
+	int newData;
+};
 
 class Sukodu
 {
@@ -19,8 +29,20 @@ private:
 	int cursorRow;
 	int cursorCol;
 
+	//历史
+	vector<HistoryOption> history;
+	vector<HistoryOption> backHistory;
+
+	//帮助
+	int helpCursorRow;
+	int helpCursorCol;
+	int helpState;
+
 	//对单个格子进行深度优先搜索
 	bool SetSingleGrid(int y, int x, int IsCreate = 1);
+
+	//行列位置合法化
+	void LegalizeAddress(int& row, int& col);
 
 public:
 	Sukodu();
@@ -45,6 +67,15 @@ public:
 
 	//更新答案
 	void UpdateAnswer();
+
+	//提示
+	void Help();
+
+	//撤销
+	void Back();
+
+	//恢复
+	void Reback();
 };
 
 //设置窗口大小
@@ -59,7 +90,7 @@ int main()
 	//创建数独
 	Sukodu s;
 	s.CreateSukodu();
-	s.SetQA(82);
+	s.SetQA(1);
 
 	s.Play();
 	return 0;
@@ -71,7 +102,7 @@ bool Sukodu::SetSingleGrid(int row, int col, int IsCreate)
 	if (!IsCreate)
 	{
 		int flag(0);
-		for (int i = col + 1; i < 9; i++)
+		for (int i = col + 1; i < 9; i++)		//对于第一行的判断
 			if (mid[row][i] == 0)
 			{
 				col = i;
@@ -100,13 +131,7 @@ bool Sukodu::SetSingleGrid(int row, int col, int IsCreate)
 	else
 	{
 		col++;
-		if (col >= 9)
-		{
-			row++;
-			col = 0;
-		}
-		if (row >= 9)
-			row = 0;
+		LegalizeAddress(row, col);
 	}
 
 	//DFS
@@ -119,7 +144,7 @@ bool Sukodu::SetSingleGrid(int row, int col, int IsCreate)
 			i = 0;
 		if (JudgeCanSet(row, col, i + 1))		//判断目标数据是否合法
 		{
-			//std::cout << row << ' ' << col << ' ' << i + 1 << endl;
+			//cout << row << ' ' << col << ' ' << i + 1 << endl;
 			mid[row][col] = i + 1;
 			if (col == 8 && row == 8)		//判断结尾
 				return true;
@@ -131,7 +156,18 @@ bool Sukodu::SetSingleGrid(int row, int col, int IsCreate)
 	return false;
 }
 
-Sukodu::Sukodu() :cursorRow(0), cursorCol(0), answer{ 0 }, question{ 0 }, mid{ 0 }, state { 0 }, haveAns(1) {}
+void Sukodu::LegalizeAddress(int& row, int& col)
+{
+	if (col >= 9)
+	{
+		row++;
+		col = 0;
+	}
+	if (row >= 9)
+		row = 0;
+}
+
+Sukodu::Sukodu() :cursorRow(0), cursorCol(0), answer{ 0 }, question{ 0 }, mid{ 0 }, state{ 0 }, haveAns(1), helpCursorCol(-1), helpCursorRow(-1), helpState(0), history(), backHistory() {}
 
 void Sukodu::CreateSukodu()
 {
@@ -143,6 +179,9 @@ void Sukodu::CreateSukodu()
 
 bool Sukodu::JudgeCanSet(int row, int col, int num)
 {
+	if (num == 0)
+		return true;
+
 	//行判断
 	for (int i = 0; i < 9; i++)
 		if (mid[row][i] == num && i != col)
@@ -170,37 +209,79 @@ void Sukodu::ShowData()
 	SetConsoleCursorPosition(hConsole, { 0,0 });
 
 	SetConsoleTextAttribute(hConsole, 0x07);
-	std::cout << " ----- ----- ----- ----- ----- ----- ----- ----- ----- " << endl;
+	cout << " ----- ----- ----- ----- ----- ----- ----- ----- ----- " << endl;
 	for (int nowRow = 0; nowRow < 9; nowRow++)
 	{
-		std::cout << "|     |     |     |     |     |     |     |     |     |" << endl;
-		std::cout << '|';
+		cout << "|     |     |     |     |     |     |     |     |     |" << endl;
+		cout << '|';
 		for (int nowCol = 0; nowCol < 9; nowCol++)
 		{
-			std::cout << ' ';
+			cout << ' ';
 			if (nowCol == cursorCol && nowRow == cursorRow)
 			{
-				if (state[nowRow][nowCol] == 1)
-					SetConsoleTextAttribute(hConsole, 0x76);
+				if (!(helpCursorCol == nowCol && helpCursorRow == nowRow))
+				{
+					if (state[nowRow][nowCol] == 1)
+						SetConsoleTextAttribute(hConsole, 0x76);
+					else
+						SetConsoleTextAttribute(hConsole, 0x70);
+					cout << ' ' << question[nowRow][nowCol] << ' ';
+				}
 				else
-					SetConsoleTextAttribute(hConsole, 0x70);
-				std::cout << ' ' << question[nowRow][nowCol] << ' ';
-				SetConsoleTextAttribute(hConsole, 0x07);
+				{
+					switch (helpState)
+					{
+					case 1:
+						SetConsoleTextAttribute(hConsole, 0x79);
+						cout << ' ' << answer[nowRow][nowCol] << ' ';
+						break;
+					case 2:
+						SetConsoleTextAttribute(hConsole, 0x7C);
+						cout << ' ' << question[nowRow][nowCol] << ' ';
+					default:
+						break;
+					}
+					helpCursorCol = -1;
+					helpCursorRow = -1;
+					helpState = 0;
+				}
 			}
 			else if (state[nowRow][nowCol] == 1)
 			{
 				SetConsoleTextAttribute(hConsole, 0x06);
-				std::cout << ' ' << question[nowRow][nowCol] << ' ';
-				SetConsoleTextAttribute(hConsole, 0x07);
+				cout << ' ' << question[nowRow][nowCol] << ' ';
 			}
 			else
-				std::cout << ' ' << question[nowRow][nowCol] << ' ';
-			std::cout << " |";
+			{
+				if (nowCol == helpCursorCol && nowRow == helpCursorRow)
+				{
+					switch (helpState)
+					{
+					case 1:
+						SetConsoleTextAttribute(hConsole, 0x97);
+						cout << ' ' << answer[nowRow][nowCol] << ' ';
+						break;
+					case 2:
+						SetConsoleTextAttribute(hConsole, 0xC7);
+						cout << ' ' << question[nowRow][nowCol] << ' ';
+					default:
+						break;
+					}
+					helpCursorCol = -1;
+					helpCursorRow = -1;
+					helpState = 0;
+				}
+				else
+					cout << ' ' << question[nowRow][nowCol] << ' ';
+			}
+			SetConsoleTextAttribute(hConsole, 0x07);
+			cout << " |";
 		}
-		std::cout << endl;
-		std::cout << "|     |     |     |     |     |     |     |     |     |" << endl;
-		std::cout << " ----- ----- ----- ----- ----- ----- ----- ----- ----- " << endl;
+		cout << endl;
+		cout << "|     |     |     |     |     |     |     |     |     |" << endl;
+		cout << " ----- ----- ----- ----- ----- ----- ----- ----- ----- " << endl;
 	}
+	cout << "1-9: set num; w,a,s,d: move cursor; c: cancal; z: back; y:reback; h: help" << endl << "                              " << endl;
 }
 
 void Sukodu::SetBlank(int num)
@@ -213,13 +294,7 @@ void Sukodu::SetBlank(int num)
 		while (question[x][y] == 0)
 		{
 			y++;
-			if (y >= 9)
-			{
-				x++;
-				y = 0;
-			}
-			if (x >= 9)
-				x = 0;
+			LegalizeAddress(x, y);
 		}
 		question[x][y] = 0;
 		state[x][y] = 0;
@@ -255,10 +330,17 @@ void Sukodu::Play()
 		case 'c':
 			if (state[cursorRow][cursorCol] != 1)
 				question[cursorRow][cursorCol] = 0;
+			history.push_back({ cursorRow,cursorCol,2,question[cursorRow][cursorCol],0 });
+			backHistory.clear();
+			UpdateAnswer();
 			break;
 		case 'd':
 			if (cursorCol < 8)
 				cursorCol++;
+			break;
+		case 'h':
+			Help();
+			flag = 1;
 			break;
 		case 's':
 			if (cursorRow < 8)
@@ -268,6 +350,14 @@ void Sukodu::Play()
 			if (cursorRow > 0)
 				cursorRow--;
 			break;
+		case 'y':
+			Reback();
+			UpdateAnswer();
+			break;
+		case 'z':
+			Back();
+			UpdateAnswer();
+			break;
 		default:
 			flag = 1;
 			break;
@@ -275,11 +365,13 @@ void Sukodu::Play()
 		if (key >= '1' && key <= '9' && state[cursorRow][cursorCol] == 0)
 		{
 			flag = 0;
+			if (question[cursorRow][cursorCol] == 0)
+				history.push_back({ cursorRow,cursorCol,1,0,key - '0' }), backHistory.clear();
+			else
+				history.push_back({ cursorRow,cursorCol,3,question[cursorRow][cursorCol],key - '0' }), backHistory.clear();
 			question[cursorRow][cursorCol] = key - '0';
 			if (key - '0' != answer[cursorRow][cursorCol])
-			{
 				UpdateAnswer();
-			}
 		}
 		if (flag)
 			continue;
@@ -301,7 +393,9 @@ void Sukodu::Play()
 			continue;
 		isExit = 1;
 	}
-	std::cout << "\nfinish";
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(hConsole, { 37,0 });
+	cout << "finish";
 }
 
 void Sukodu::UpdateAnswer()
@@ -323,6 +417,83 @@ void Sukodu::UpdateAnswer()
 			for (int j = 0; j < 9; j++)
 				answer[i][j] = mid[i][j];
 	}
+}
+
+void Sukodu::Help()
+{
+	if (haveAns)
+	{
+		int row(rand() % 9), col(rand() % 9);
+		while (question[row][col] != 0)
+		{
+			col++;
+			LegalizeAddress(row, col);
+		}
+		helpCursorCol = col;
+		helpCursorRow = row;
+		helpState = 1;
+		ShowData();
+	}
+	else
+		cout << "no answer" << endl;
+}
+
+void Sukodu::Back()
+{
+	if (history.size() == 0)
+	{
+		ShowData();
+		cout << "cannot back" << endl;
+		return;
+	}
+	vector<HistoryOption>::iterator iter(history.end() - 1);
+	switch ((*iter).option)
+	{
+	case 1:
+		question[(*iter).row][(*iter).col] = 0;
+		break;
+	case 2:
+		question[(*iter).row][(*iter).col] = (*iter).oldData;
+		break;
+	case 3:
+		question[(*iter).row][(*iter).col] = (*iter).oldData;
+	default:
+		break;
+	}
+	helpCursorCol = (*iter).col;
+	helpCursorRow = (*iter).row;
+	helpState = 2;
+	backHistory.push_back((*iter));
+	history.pop_back();
+}
+
+void Sukodu::Reback()
+{
+	if (backHistory.size() == 0)
+	{
+		ShowData();
+		cout << "cannot reback" << endl;
+		return;
+	}
+	vector<HistoryOption>::iterator iter(backHistory.end() - 1);
+	switch ((*iter).option)
+	{
+	case 1:
+		question[(*iter).row][(*iter).col] = (*iter).newData;
+		break;
+	case 2:
+		question[(*iter).row][(*iter).col] = (*iter).newData;
+		break;
+	case 3:
+		question[(*iter).row][(*iter).col] = 0;
+	default:
+		break;
+	}
+	helpCursorCol = (*iter).col;
+	helpCursorRow = (*iter).row;
+	helpState = 2;
+	history.push_back((*iter));
+	backHistory.pop_back();
 }
 
 void SetWindowsSize(int height, int width)
